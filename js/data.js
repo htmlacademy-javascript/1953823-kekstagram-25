4a2be393ad7f63bede3ff82770b78b257e60dc2a
Maria <korzun.mariya@yandex.ru>
2022-04-02 23:44:56 +0300

    Отрисуй меня полностью (1)

--- js/data.js
+++ js/data.js
@@ -1,76 +1,10 @@
-import {getRandomPositiveInteger, getRandomArrayElement, generateNumbers} from './util.js';
-
-const ID = generateNumbers(1,25);
-
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-  'Анна',
-  'Максим',
-  'Нина',
-  'Вячеслав',
-  'Ксения',
-  'Антонина',
-];
-
-const DESCRIPTIONS = [
-  'Пляж',
-  'Указатель',
-  'Океан',
-  'Девушка на пляже',
-  'Рисовые человечки',
-  'Машина мечты',
-  'Клубнчки',
-  'Морс',
-  'Самолет',
-  'Обувь',
-  'Дорога к морю',
-  'Авто',
-  'Здоровое питание',
-  'Кексоролл',
-  'Тапочки',
-  'Небо',
-  'Хор',
-  'Ретро-авто',
-  'Светлячки',
-  'Пальмы',
-  'Вкуснотища',
-  'Закат',
-  'Краб',
-  'Концерт',
-  'Переправа',
-];
+import {getRandomPositiveInteger, getRandomArrayElement} from './util.js';
+import {MESSAGES, DESCRIPTIONS, NAMES} from './constants.js';
 
 const PHOTO_AMOUNT = 25;
 
-const getCommentId = (min, max, number) => {
-  const IDS = [];
-  while (IDS.length < number) {
-    const commentId = getRandomPositiveInteger(min, max);
-    if (!IDS.includes(commentId)) {
-      IDS.push(commentId);
-    }
-  }
-  return IDS;
-};
-
-const createComment = () => ({
-  id: getRandomArrayElement(getCommentId(1000, 2000, 100)),
+const createComment = (_, number) => ({
+  id: number,
   avatar: `img/avatar-${  getRandomPositiveInteger(1, 6)  }.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
@@ -78,9 +12,9 @@ const createComment = () => ({
 
 const SIMILAR_COMMENTS_AMOUNT = 5;
 
-const createObject = () => ({
-  id: getRandomArrayElement(ID),
-  url: `photos/${  getRandomArrayElement(ID)  }.jpg`,
+const createObject = (_, number) => ({
+  id: number + 1,
+  url: `photos/${number + 1}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomPositiveInteger(15, 200),
   comments: Array.from({length: SIMILAR_COMMENTS_AMOUNT}, createComment),
