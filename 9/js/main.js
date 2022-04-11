2330af46a72d8aebe121feafd593cd5c7e661659
Keks <keks@htmlacademy.ru>
2022-04-12 00:43:59 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,33 @@
+import './load-image.js';
+import './validation.js';
+import {photoDescriptions} from './data.js';
+import {generateCard} from './photos.js';
+import {openBigPicture} from './big-picture.js';
+import {closeBigPicture} from './util.js';
+
+const photosWrapper = document.querySelector('.pictures');
+
+photoDescriptions.forEach((photo) => {
+  const photoNode = generateCard(photo);
+  photoNode.addEventListener('click', () => {
+    openBigPicture(photo);
+  });
+  photosWrapper.append(photoNode);
+});
+
+const closeBigPictureClick = (evt) => {
+  const element = evt.target;
+  if (element.closest('.cancel')) {
+    closeBigPicture();
+  }
+};
+
+const closeBigPictureEsc = (evt) => {
+  const key = evt.keyCode;
+  if (key === 27) {
+    closeBigPicture();
+  }
+};
+
+document.addEventListener('click', closeBigPictureClick);
+document.addEventListener('keydown', closeBigPictureEsc);
