ae7e9bce2c454784f401356c27c505777263d2e0
Maria <korzun.mariya@yandex.ru>
2022-04-13 13:28:45 +0300

    Правда или действие (часть 2)

--- js/validation.js
+++ js/validation.js
@@ -3,10 +3,15 @@ const textHashtags = form.querySelector('.text__hashtags');
 const comments = form.querySelector('.text__description');
 const MAX_SYMBOLS = 140;
 
+const hash1 = 'Хэштег должен начинаться символа # (решётка), строка после решётки должна состоять из букв и чисел (не более 20 символов) и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.';
+const hash2 = 'Нельзя указать больше пяти хэш-тегов';
+const hash3 = 'Один и тот же хэш-тег не может быть использован дважды';
+const hash4 = 'Длина комментария не может составлять больше 140 символов';
+
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
-  errorClass: 'img-upload__text--invalid',
-  successClass: 'img-upload__text--valid',
+  errorTextParent: 'img-upload__text',
+  errorTextClass: 'img-upload__error-text',
 }, true);
 
 // Функция для превращения набора хэштегов в массив, вызываем метод filter() на массиве hashtagText и передаём функцию, которая отрабатывает на каждом элементе этого массива
@@ -19,7 +24,7 @@ const validateHashtags = (value) => {
   return !newHashtags.includes(false);
 };
 
-pristine.addValidator(textHashtags, validateHashtags, 'Хэштег должен начинаться символа # (решётка), строка после решётки должна состоять из букв и чисел (не более 20 символов) и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
+pristine.addValidator(textHashtags, validateHashtags, hash1);
 
 const amountHashtags = (value) => {
   const hashtag = createHashtagsArray(value);
@@ -27,7 +32,7 @@ const amountHashtags = (value) => {
   return correctAmmount;
 };
 
-pristine.addValidator(textHashtags, amountHashtags, 'Нельзя указать больше пяти хэш-тегов');
+pristine.addValidator(textHashtags, amountHashtags, hash2);
 
 const duplicatHashtags = (value) => {
   const hashtag = createHashtagsArray(value);
@@ -35,11 +40,11 @@ const duplicatHashtags = (value) => {
   return hasUniqueHashtags;
 };
 
-pristine.addValidator(textHashtags, duplicatHashtags, 'Один и тот же хэш-тег не может быть использован дважды');
+pristine.addValidator(textHashtags, duplicatHashtags, hash3);
 
 const validateComments = () => comments.length <= MAX_SYMBOLS;
 
-pristine.addValidator(comments, validateComments, 'Длина комментария не может составлять больше 140 символов');
+pristine.addValidator(comments, validateComments, hash4);
 
 const onFormValidation = () => {
   form.addEventListener('submit', (evt) => {
