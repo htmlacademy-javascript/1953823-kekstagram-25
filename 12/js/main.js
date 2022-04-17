fbb7afaf59870fa86ce6e5edf72d7a38f9763bfb
Keks <keks@htmlacademy.ru>
2022-04-17 16:41:20 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,39 @@
+import './load-image.js';
+import './scale.js';
+import './effects.js';
+import {photoDescriptions} from './data.js';
+import {generateCard} from './photos.js';
+import {openBigPicture} from './big-picture.js';
+import {closeBigPicture} from './util.js';
+import {getData} from './api.js';
+
+const photosWrapper = document.querySelector('.pictures');
+
+const renderPictures = photoDescriptions.forEach((photo) => {
+  const photoNode = generateCard(photo);
+  photoNode.addEventListener('click', () => {
+    openBigPicture(photo);
+  });
+  photosWrapper.append(photoNode);
+});
+
+export const showGallery = () => {
+  getData(renderPictures);
+};
+
+const closeBigPictureClick = (evt) => {
+  const element = evt.target;
+  if (element.closest('.cancel')) {
+    closeBigPicture();
+  }
+};
+
+const closeBigPictureEsc = (evt) => {
+  const key = evt.keyCode;
+  if (key === 27) {
+    closeBigPicture();
+  }
+};
+
+document.addEventListener('click', closeBigPictureClick);
+document.addEventListener('keydown', closeBigPictureEsc);
