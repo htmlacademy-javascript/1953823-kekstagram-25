4a46accd7222d1b3770784dcedcaf72c7c0cd2ac
Maria <korzun.mariya@yandex.ru>
2022-04-18 19:26:02 +0300

    Финальные правки

--- js/big-picture.js
+++ js/big-picture.js
@@ -11,7 +11,7 @@ const socialComments = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const closeBigPictureButton = bigPicture.querySelector('.cancel');
 
-let i = 5;
+let elementsAmount = 5;
 
 const generateCommentList = (src, alt, commentText) => {
   const comment = document.createElement('li');
@@ -59,32 +59,32 @@ export const openBigPicture = ({description, comments, likes, url}) => {
 
 commentsLoader.addEventListener('click', () => {
   const elements = social.children;
-  if (elements.length-i <= 5) {
+  if (elements.length-elementsAmount <= 5) {
     socialComments.textContent = `${elements.length  } из ${  elements.length} комментариев`;
     commentsLoader.classList.add('hidden');
-    for (let j=elements.length-i; j>0; j--) {
+    for (let j=elements.length-elementsAmount; j>0; j--) {
       const el = elements[elements.length-j];
       el.style.display = 'flex';
     }
   }
   else {
-    socialComments.textContent = `${i+5  } из ${  elements.length} комментариев`;
-    const el1 = elements[i];
-    const el2 = elements[i+1];
-    const el3 = elements[i+2];
-    const el4 = elements[i+3];
-    const el5 = elements[i+4];
+    socialComments.textContent = `${elementsAmount+5  } из ${  elements.length} комментариев`;
+    const el1 = elements[elementsAmount];
+    const el2 = elements[elementsAmount+1];
+    const el3 = elements[elementsAmount+2];
+    const el4 = elements[elementsAmount+3];
+    const el5 = elements[elementsAmount+4];
     el1.style.display = 'flex';
     el2.style.display = 'flex';
     el3.style.display = 'flex';
     el4.style.display = 'flex';
     el5.style.display = 'flex';
   }
-  i+=5;
+  elementsAmount+=5;
 });
 
 closeBigPictureButton.addEventListener('click', () => {
-  i = 5;
+  elementsAmount = 5;
 });
 
 export {body, bigPicture, closeBigPictureButton};
